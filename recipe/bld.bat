b2c692089638d026662776234a1bf2e5f6970716
Andrii Osipov <aosipov@anaconda.com>
2026-05-26 05:02:07 -0700

    PKG-13856: utf8proc 2.11.3

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,4 @@
+@echo on
 
 mkdir build
 if errorlevel 1 exit 1
@@ -10,9 +11,9 @@ cmake -G "NMake Makefiles" ^
   -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON ^
   -DBUILD_SHARED_LIBS:BOOL=ON ^
   "%SRC_DIR%"
-
 if errorlevel 1 exit 1
-cmake --build . --config Release
-cmake --build . --target install --config Release
 
-exit 0
+cmake --build . --config Release
+if errorlevel 1 exit 1
+cmake --build . --target install --config Release
+if errorlevel 1 exit 1
